4d8bf712925f5be34b42f6e8ec5db621966f8fba
khchoi0301 <khchoi0301@gmail.com>
2020-03-29 01:44:58 +0900

    add validateSignup and signup function on authcontroller.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,48 @@
 const mongoose = require('mongoose');
 const User = mongoose.model("User");
 
-exports.validateSignup = () => { };
+exports.validateSignup = (req, res, next) => {
+    // express-validator가 middleware로 적용됨
+    req.sanitizeBody('name')
+    req.sanitizeBody('email')
+    req.sanitizeBody('password')
+    // Hello world :>)  to  Hello world :&gt;)
 
-exports.signup = () => { };
+    // Name is non-null and is 4 to 10 characters
+    req.checkBody("name", "Enter a name").notEmpty();
+    req.checkBody("name", "Name must be between 4 and 10 character")
+        .isLength({ min: 4, max: 10 });
+
+    // Email is non-null, valid, and normalized
+    req.checkBody("email", "Enter a valid email")
+        .isEmail()
+        .normalizeEmail()
+
+    // Password must be non-null between 4 and 10 characters
+    req.checkBody("password", "Enter a password").notEmpty();
+    req.checkBody("password", "password must be between 4 and 10 character")
+        .isLength({ min: 4, max: 10 });
+
+    const errors = req.validationErrors();
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).send(firstError);
+    }
+    next()
+};
+
+exports.signup = async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await new User({ name, email, password });
+
+    await User.register(user, password, (err, user) => {
+        console.log(err, user)
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+        res.json(user)
+    })
+};
 
 exports.signin = () => { };
 
