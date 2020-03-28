56dbff789945f17341b6d7eb1f1afea9285788f2
khchoi0301 <khchoi0301@gmail.com>
2020-03-29 01:43:52 +0900

    inital commit

---
+++ server/controllers/authController.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
+
+exports.validateSignup = () => { };
+
+exports.signup = () => { };
+
+exports.signin = () => { };
+
+exports.signout = () => { };
+
+exports.checkAuth = () => { };
